3f79d8fc94531495aab1e36d8997720ef2e02241
enzojpn <enzo_jpn@hotmail.com>
2023-01-28 22:26:21 -0300

    add categories controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog2811.Controllers
+{
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("categories")]
+        public IActionResult Get([FromServices] BlogDataContext context)
+        {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+
+    }
+}
